235554000f30bb66bdbbc998c15db1a27d23e4d7
Nicoforcio <nicolasbacon.nb@gmail.com>
2022-07-06 12:08:36 +0200

    Bootstrap + fontawesome base + header + footer + nav

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Participant;
 use App\Entity\Sortie;
+use App\Form\ParticipantType;
+use App\Repository\ParticipantRepository;
 use App\Service\InscriptionService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,11 +18,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class ParticipantController extends AbstractController
 {
 
-    #[Route('/{id}', name: 'profil')]
+    #[Route('/{id}', name: 'profil', methods: ['GET'])]
     public function profil(Participant $participant): Response
     {
-        return $this->render('participant/profil.html.twig', [
+        return $this->render('participant/show.html.twig', [
             'participant' => $participant,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'editer_profil', methods: ['GET', 'POST'])]
+    public function edit(Request $request, int $id, ParticipantRepository $participantRepository): Response
+    {
+        if($this->getUser()->getId() != $id)
+        throw new AccessDeniedException("Vous ne pouvez pas modifer un profil qui n'est pas le votre");
+        $participant = $participantRepository->find($id);
+
+        $form = $this->createForm(ParticipantType::class, $participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $participantRepository->add($participant, true);
+
+            return $this->redirectToRoute('participant_profil', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('participant/edit.html.twig', [
+            'participant' => $participant,
+            'form' => $form,
         ]);
     }
 
